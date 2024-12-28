9f530490d7c0353d6f3c485861637cc3b523bfd8
hery tsitohaina <herytinara8@gmail.com>
2024-12-28 08:29:44 +0100

    improve : add header

---
+++ src/components/Header.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-app-bar
+    :elevation="7"
+    scroll-behavior="elevate inverted fade-image"
+    scroll-threshold="290"
+    image="https://picsum.photos/1920/1080?random"
+  >
+    <template #prepend>
+      <v-app-bar-nav-icon />
+    </template>
+
+    <v-app-bar-title>Web mapping VDI</v-app-bar-title>
+
+    <template #append>
+      <v-btn icon="mdi-heart" />
+
+      <v-btn icon="mdi-magnify" />
+
+      <v-btn icon="mdi-dots-vertical" />
+    </template>
+  </v-app-bar>
+</template>
+
+<script setup lang="ts">
+</script>
+<style>
+</style>
